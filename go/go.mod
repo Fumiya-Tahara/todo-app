438a8f9ecc83df6f85b64e5283aadd7002f0cd8d
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-04-16 15:22:35 +0900

    env読み込み

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module github.com/Fumiya-Tahara/todo-app
+
+go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.0
--- go/main.go
+++ go/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
 
 func main() {
-	fmt.Println("Hello, World!")
+	dbConf := os.Getenv("MYSQL_USER") + ":" + os.Getenv("MYSQL_PASSWORD") + "@tcp(127.0.0.1:3306)/" + os.Getenv("MYSQL_DATABASE")
+
+	var err error
+	db, err = sql.Open("mysql", dbConf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
 }
