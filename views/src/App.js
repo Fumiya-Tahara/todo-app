e7ee41cb5b8a3d0c9f6a7e87e12c3b347df6722f
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-05-17 14:41:40 +0900

    view

--- views/src/App.js
+++ views/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import { useState, useRef } from 'react';
 import TodoList from './components/js/TodoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const todoNameRef = useRef();
+  
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), name: name, completed: false }]
+    })
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos)
+  }
+
   return (
       <div>
-        <TodoList />
+        <TodoList todos={todos} toggleTodo={toggleTodo}/>
+        <label htmlFor="title">title</label>
+        <input type="text" ref={todoNameRef} />
+        <button onClick={handleAddTodo}>タスクを追加する</button>
+        <button onClick={handleClear}>完了したタスクの削除</button>
+        <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
       </div>
     );
 }
--- views/src/components/js/TodoList.js
+++ views/src/components/js/TodoList.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import Todo from './Todo';
 
-const TodoList = () => {
-  return (
-    <div>TodoList</div>
-  )
+const TodoList = ({todos, toggleTodo}) => {
+  return todos.map((todo) => <Todo todo={todo} toggleTodo={toggleTodo} key={todo.id}/>)
 }
 
 export default TodoList;
