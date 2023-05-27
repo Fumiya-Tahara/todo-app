67697ae478bae6f25fc88d422bb99e7ff493e46e
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-05-28 08:35:04 +0900

    全てのtodoをGETするAPI

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -15,14 +15,21 @@ func main() {
 	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "GET")
-		taskHandler := &domain.TaskStorage{
-			DB: client.DB,
+		if r.Method == "GET" {
+			w.WriteHeader(http.StatusOK)
+			taskHandler := &domain.TaskStorage{
+				DB: client.DB,
+			}
+			data, err := taskHandler.GetTasks()
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(string(data))
+			w.Write(data)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("Method not allowed"))
 		}
-		data, err := taskHandler.GetTasks()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(data))
 	})
 
 	fmt.Println("Server started at http://localhost:8080")
