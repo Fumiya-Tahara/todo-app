3e6b5cc89b534b1b2050e54a2afb952dd896f82c
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-05-31 00:00:25 +0900

    handler呼び出し

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -4,31 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/Fumiya-Tahara/todo-app/internal/infrastracture"
+	"github.com/Fumiya-Tahara/todo-app/handler"
 )
 
 func main() {
-	client := infrastracture.NewStorage()
-	defer client.DB.Close()
-	// http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	// 	w.Header().Set("Access-Control-Allow-Methods", "GET")
-	// 	if r.Method == "GET" {
-	// 		w.WriteHeader(http.StatusOK)
-	// 		taskHandler := &domain.TaskStorage{
-	// 			DB: client.DB,
-	// 		}
-	// 		data, err := taskHandler.GetTasks()
-	// 		if err != nil {
-	// 			log.Fatal(err)
-	// 		}
-	// 		fmt.Println(string(data))
-	// 		w.Write(data)
-	// 	} else {
-	// 		w.WriteHeader(http.StatusMethodNotAllowed)
-	// 		w.Write([]byte("Method not allowed"))
-	// 	}
-	// })
+	http.HandleFunc("/tasks", handler.GetTaskList())
 
 	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
--- api/internal/handler/task.go
+++ api/internal/handler/task.go
@@ -1,21 +1,43 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/Fumiya-Tahara/todo-app/generated/openapi"
+	"github.com/Fumiya-Tahara/todo-app/internal/domain"
+	"github.com/Fumiya-Tahara/todo-app/internal/infrastracture"
 )
 
 type handler struct{}
 
-func NewHandler() ServerInterface {
-	return &handler
+func NewHandler() openapi.ServerInterface {
+	return &handler{}
 }
 
 func (h handler) GetTaskList(w http.ResponseWriter, r *http.Request) {
-	log.Println("成功")
-	return nil
+	client := infrastracture.NewStorage()
+	defer client.DB.Close()
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+	if r.Method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		taskHandler := &domain.TaskStorage{
+			DB: client.DB,
+		}
+		data, err := taskHandler.GetTasks()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(data))
+		w.Write(data)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method not allowed"))
+	}
 }
 
 func (h handler) GetTasksId(w http.ResponseWriter, r *http.Request, id int) {
-	return nil
+	log.Println("GetTaskId呼び出し成功")
 }
