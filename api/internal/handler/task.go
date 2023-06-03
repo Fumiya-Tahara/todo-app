13827bc19777d6b622cbf26ef35b0fbefcaf145a
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-06-03 11:29:38 +0900

    path parameters

--- api/internal/handler/task.go
+++ api/internal/handler/task.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/Fumiya-Tahara/todo-app/generated/openapi"
 	"github.com/Fumiya-Tahara/todo-app/internal/domain"
@@ -21,15 +22,21 @@ func (h handler) GetTaskList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "GET")
 	if r.Method == "GET" {
-		w.WriteHeader(http.StatusOK)
-		taskHandler := &domain.TaskStorage{
-			DB: client.DB,
+		path := r.URL.Path
+		id := strings.TrimPrefix(path, "/tasks")
+		if id == "" {
+			w.WriteHeader(http.StatusOK)
+			taskHandler := &domain.TaskStorage{
+				DB: client.DB,
+			}
+			data, err := taskHandler.GetTasks()
+			if err != nil {
+				log.Fatal(err)
+			}
+			w.Write(data)
 		}
-		data, err := taskHandler.GetTasks()
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.Write(data)
+		// idがあった場合の処理
+
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("Method not allowed"))
