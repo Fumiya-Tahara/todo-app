59b3757ebdbb82a7054a9a3380f9cc7cd03c4fd1
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-06-22 15:10:07 +0900

    introduced next.js

--- views/src/components/js/TodoList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Todo from './Todo';
-
-const TodoList = ({todos, toggleTodo}) => {
-  return todos.map((todo) => <Todo todo={todo} toggleTodo={toggleTodo} key={todo.id}/>)
-}
-
-export default TodoList;
