02b7c371e799f42893a78b1785e494d52a79c93d
Fumiya Tahara <taharafumiya0219@gmail.com>
2023-07-10 17:00:19 +0900

    グリッドシステム

--- views/src/components/Todo.js
+++ views/src/components/Todo.js
@@ -1,15 +1,20 @@
 import Checkbox from '@mui/material/Checkbox';
-import { Link } from '@mui/material';
+import { Link, Grid } from '@mui/material';
 
 function Todo() {
   return (
     <div>
-      <div>
-        <Checkbox />
-        <Link >
-          <span>タイトルを書く</span>
-        </Link>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={3} justifyContent={"center"} alignContent={"center"}>
+          <Checkbox />
+          <Link >
+            <span>タイトルを書く</span>
+          </Link>
+        </Grid>
+        <Grid item xs={2}>
+          <div>20XX年○月×日</div>
+        </Grid>
+      </Grid>
     </div>
   )
 }
